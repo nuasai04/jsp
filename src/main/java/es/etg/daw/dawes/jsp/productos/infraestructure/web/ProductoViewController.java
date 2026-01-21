e56b382725e56a20475c03052709518a3c7d7483
Nuhaila <nuhailaassaid16@gmail.com>
2026-01-21 10:39:15 -0600

    cambiio el ProductoViewController para agregar las enumeraciones

--- src/main/java/es/etg/daw/dawes/jsp/productos/infraestructure/web/ProductoViewController.java
+++ src/main/java/es/etg/daw/dawes/jsp/productos/infraestructure/web/ProductoViewController.java
@@ -5,13 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 import es.etg.daw.dawes.jsp.productos.application.service.FindProductoService;
+import es.etg.daw.dawes.jsp.productos.infraestructure.web.constants.WebRoutes;
+import es.etg.daw.dawes.jsp.productos.infraestructure.web.enums.JspView;
+import es.etg.daw.dawes.jsp.productos.infraestructure.web.enums.ModelAttribute;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/web/productos")
+@RequestMapping(WebRoutes.PRODUCTOS_BASE)
 public class ProductoViewController {
     
     private final FindProductoService findProductoService;
@@ -19,7 +21,7 @@ public class ProductoViewController {
     @GetMapping
     public String listar(Model model) {
         // Consulto todos los productos y los meto en un atributo del modelo para poder acceder a ellos en la JSP.
-        model.addAttribute("productos", findProductoService.findAll());
-        return "productos-lista"; // Plantilla JSP a utilizar
+        model.addAttribute(ModelAttribute.PRODUCT_LIST.getName(), findProductoService.findAll());
+        return JspView.PRODUCT_LIST.getPath(); // Busca productos-lista.jsp
     }
 }
