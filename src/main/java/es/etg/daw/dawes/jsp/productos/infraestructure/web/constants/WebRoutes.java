76b991f2a5d1a109b72552f04088424833e39151
Nuhaila <nuhailaassaid16@gmail.com>
2026-01-21 10:35:54 -0600

    Creacion de la interfaz WebRoutes en infraestructure.web.constants

---
+++ src/main/java/es/etg/daw/dawes/jsp/productos/infraestructure/web/constants/WebRoutes.java
@@ -0,0 +1,8 @@
+package es.etg.daw.dawes.jsp.productos.infraestructure.web.constants;
+
+/**
+ * Contiene las rutas de acceso desde el navegador
+ */
+public interface WebRoutes {
+    public static final String PRODUCTOS_BASE = "/web/productos";
+}
